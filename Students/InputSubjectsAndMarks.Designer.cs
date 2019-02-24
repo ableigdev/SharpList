3008a108f4fbf5760fd135b8f9a4b7b3e963dbc7
ableigdev <igor.yarovoy.98@ukr.net>
2019-02-24 23:35:54 +0200

    Add the window for adding general subjects and marks

---
+++ Students/InputSubjectsAndMarks.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace Students
+{
+    partial class InputSubjectsAndMarks
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GroupBox_Add_Data = new System.Windows.Forms.GroupBox();
+            this.Label_Subject = new System.Windows.Forms.Label();
+            this.Label_Mark = new System.Windows.Forms.Label();
+            this.TextBox_Input_Subject = new System.Windows.Forms.TextBox();
+            this.TextBox_Input_Mark = new System.Windows.Forms.TextBox();
+            this.Button_Add_Subject = new System.Windows.Forms.Button();
+            this.Button_Add_Mark = new System.Windows.Forms.Button();
+            this.Label_All_Subjects = new System.Windows.Forms.Label();
+            this.Label_All_Marks = new System.Windows.Forms.Label();
+            this.ListBox_All_Subjects = new System.Windows.Forms.ListBox();
+            this.ListBox_All_Marks = new System.Windows.Forms.ListBox();
+            this.Button_Save_Subjects = new System.Windows.Forms.Button();
+            this.Button_Delete_Subject = new System.Windows.Forms.Button();
+            this.Button_Delete_Mark = new System.Windows.Forms.Button();
+            this.GroupBox_Add_Data.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // GroupBox_Add_Data
+            // 
+            this.GroupBox_Add_Data.Controls.Add(this.Button_Add_Mark);
+            this.GroupBox_Add_Data.Controls.Add(this.Button_Add_Subject);
+            this.GroupBox_Add_Data.Controls.Add(this.TextBox_Input_Mark);
+            this.GroupBox_Add_Data.Controls.Add(this.TextBox_Input_Subject);
+            this.GroupBox_Add_Data.Controls.Add(this.Label_Mark);
+            this.GroupBox_Add_Data.Controls.Add(this.Label_Subject);
+            this.GroupBox_Add_Data.Location = new System.Drawing.Point(13, 13);
+            this.GroupBox_Add_Data.Name = "GroupBox_Add_Data";
+            this.GroupBox_Add_Data.Size = new System.Drawing.Size(357, 77);
+            this.GroupBox_Add_Data.TabIndex = 0;
+            this.GroupBox_Add_Data.TabStop = false;
+            this.GroupBox_Add_Data.Text = "Add data";
+            // 
+            // Label_Subject
+            // 
+            this.Label_Subject.AutoSize = true;
+            this.Label_Subject.Location = new System.Drawing.Point(6, 20);
+            this.Label_Subject.Name = "Label_Subject";
+            this.Label_Subject.Size = new System.Drawing.Size(46, 13);
+            this.Label_Subject.TabIndex = 0;
+            this.Label_Subject.Text = "Subject:";
+            // 
+            // Label_Mark
+            // 
+            this.Label_Mark.AutoSize = true;
+            this.Label_Mark.Location = new System.Drawing.Point(6, 50);
+            this.Label_Mark.Name = "Label_Mark";
+            this.Label_Mark.Size = new System.Drawing.Size(34, 13);
+            this.Label_Mark.TabIndex = 3;
+            this.Label_Mark.Text = "Mark:";
+            // 
+            // TextBox_Input_Subject
+            // 
+            this.TextBox_Input_Subject.Location = new System.Drawing.Point(76, 17);
+            this.TextBox_Input_Subject.Name = "TextBox_Input_Subject";
+            this.TextBox_Input_Subject.Size = new System.Drawing.Size(194, 20);
+            this.TextBox_Input_Subject.TabIndex = 1;
+            // 
+            // TextBox_Input_Mark
+            // 
+            this.TextBox_Input_Mark.Location = new System.Drawing.Point(76, 43);
+            this.TextBox_Input_Mark.Name = "TextBox_Input_Mark";
+            this.TextBox_Input_Mark.Size = new System.Drawing.Size(194, 20);
+            this.TextBox_Input_Mark.TabIndex = 4;
+            // 
+            // Button_Add_Subject
+            // 
+            this.Button_Add_Subject.Location = new System.Drawing.Point(276, 15);
+            this.Button_Add_Subject.Name = "Button_Add_Subject";
+            this.Button_Add_Subject.Size = new System.Drawing.Size(75, 23);
+            this.Button_Add_Subject.TabIndex = 2;
+            this.Button_Add_Subject.Text = "Add &Subject";
+            this.Button_Add_Subject.UseVisualStyleBackColor = true;
+            // 
+            // Button_Add_Mark
+            // 
+            this.Button_Add_Mark.Location = new System.Drawing.Point(276, 45);
+            this.Button_Add_Mark.Name = "Button_Add_Mark";
+            this.Button_Add_Mark.Size = new System.Drawing.Size(75, 23);
+            this.Button_Add_Mark.TabIndex = 5;
+            this.Button_Add_Mark.Text = "Add &Mark";
+            this.Button_Add_Mark.UseVisualStyleBackColor = true;
+            // 
+            // Label_All_Subjects
+            // 
+            this.Label_All_Subjects.AutoSize = true;
+            this.Label_All_Subjects.Location = new System.Drawing.Point(13, 97);
+            this.Label_All_Subjects.Name = "Label_All_Subjects";
+            this.Label_All_Subjects.Size = new System.Drawing.Size(51, 13);
+            this.Label_All_Subjects.TabIndex = 6;
+            this.Label_All_Subjects.Text = "Subjects:";
+            // 
+            // Label_All_Marks
+            // 
+            this.Label_All_Marks.AutoSize = true;
+            this.Label_All_Marks.Location = new System.Drawing.Point(277, 97);
+            this.Label_All_Marks.Name = "Label_All_Marks";
+            this.Label_All_Marks.Size = new System.Drawing.Size(39, 13);
+            this.Label_All_Marks.TabIndex = 7;
+            this.Label_All_Marks.Text = "Marks:";
+            // 
+            // ListBox_All_Subjects
+            // 
+            this.ListBox_All_Subjects.FormattingEnabled = true;
+            this.ListBox_All_Subjects.Location = new System.Drawing.Point(13, 120);
+            this.ListBox_All_Subjects.Name = "ListBox_All_Subjects";
+            this.ListBox_All_Subjects.Size = new System.Drawing.Size(258, 160);
+            this.ListBox_All_Subjects.TabIndex = 8;
+            // 
+            // ListBox_All_Marks
+            // 
+            this.ListBox_All_Marks.FormattingEnabled = true;
+            this.ListBox_All_Marks.Location = new System.Drawing.Point(277, 120);
+            this.ListBox_All_Marks.Name = "ListBox_All_Marks";
+            this.ListBox_All_Marks.Size = new System.Drawing.Size(93, 160);
+            this.ListBox_All_Marks.TabIndex = 9;
+            // 
+            // Button_Save_Subjects
+            // 
+            this.Button_Save_Subjects.Location = new System.Drawing.Point(12, 286);
+            this.Button_Save_Subjects.Name = "Button_Save_Subjects";
+            this.Button_Save_Subjects.Size = new System.Drawing.Size(112, 23);
+            this.Button_Save_Subjects.TabIndex = 10;
+            this.Button_Save_Subjects.Text = "&Save";
+            this.Button_Save_Subjects.UseVisualStyleBackColor = true;
+            // 
+            // Button_Delete_Subject
+            // 
+            this.Button_Delete_Subject.Enabled = false;
+            this.Button_Delete_Subject.Location = new System.Drawing.Point(135, 286);
+            this.Button_Delete_Subject.Name = "Button_Delete_Subject";
+            this.Button_Delete_Subject.Size = new System.Drawing.Size(112, 23);
+            this.Button_Delete_Subject.TabIndex = 11;
+            this.Button_Delete_Subject.Text = "Delete S&ubject";
+            this.Button_Delete_Subject.UseVisualStyleBackColor = true;
+            // 
+            // Button_Delete_Mark
+            // 
+            this.Button_Delete_Mark.Enabled = false;
+            this.Button_Delete_Mark.Location = new System.Drawing.Point(258, 286);
+            this.Button_Delete_Mark.Name = "Button_Delete_Mark";
+            this.Button_Delete_Mark.Size = new System.Drawing.Size(112, 23);
+            this.Button_Delete_Mark.TabIndex = 12;
+            this.Button_Delete_Mark.Text = "Delete M&ark";
+            this.Button_Delete_Mark.UseVisualStyleBackColor = true;
+            // 
+            // InputSubjectsAndMarks
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(382, 322);
+            this.Controls.Add(this.Button_Delete_Mark);
+            this.Controls.Add(this.Button_Delete_Subject);
+            this.Controls.Add(this.Button_Save_Subjects);
+            this.Controls.Add(this.ListBox_All_Marks);
+            this.Controls.Add(this.ListBox_All_Subjects);
+            this.Controls.Add(this.Label_All_Marks);
+            this.Controls.Add(this.Label_All_Subjects);
+            this.Controls.Add(this.GroupBox_Add_Data);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(398, 361);
+            this.MinimumSize = new System.Drawing.Size(398, 361);
+            this.Name = "InputSubjectsAndMarks";
+            this.Text = "Input Subjects And Marks";
+            this.GroupBox_Add_Data.ResumeLayout(false);
+            this.GroupBox_Add_Data.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox GroupBox_Add_Data;
+        private System.Windows.Forms.Button Button_Add_Mark;
+        private System.Windows.Forms.Button Button_Add_Subject;
+        private System.Windows.Forms.TextBox TextBox_Input_Mark;
+        private System.Windows.Forms.TextBox TextBox_Input_Subject;
+        private System.Windows.Forms.Label Label_Mark;
+        private System.Windows.Forms.Label Label_Subject;
+        private System.Windows.Forms.Label Label_All_Subjects;
+        private System.Windows.Forms.Label Label_All_Marks;
+        private System.Windows.Forms.ListBox ListBox_All_Subjects;
+        private System.Windows.Forms.ListBox ListBox_All_Marks;
+        private System.Windows.Forms.Button Button_Save_Subjects;
+        private System.Windows.Forms.Button Button_Delete_Subject;
+        private System.Windows.Forms.Button Button_Delete_Mark;
+    }
+}
