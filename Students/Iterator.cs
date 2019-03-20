bb3fdb4a74fbfb4181655d7eaf9b0f59f90d8f2a
ableigdev <igor.yarovoy.98@ukr.net>
2019-03-20 21:27:44 +0200

    Add the first version of the iterator

---
+++ Students/Iterator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Students
+{
+    class Iterator<T> where T : NameList<T>, new()
+    {
+        public Iterator(NameList<T> list)
+        {
+            m_List = list;
+        }
+
+        public NameList<T> next()
+        {
+            return ++m_List;
+        }
+
+        public NameList<T> prev()
+        {
+            return --m_List;
+        }
+
+        public NameList<T> get()
+        {
+            return m_List;
+        }
+
+        private NameList<T> m_List;
+    }
+}
