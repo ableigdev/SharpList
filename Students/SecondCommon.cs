3e805a3e49a0e18294069119766b2dfb6fd5d44e
ableigdev <igor.yarovoy.98@ukr.net>
2019-03-21 23:44:40 +0200

    Add a common function for the iteration

---
+++ Students/SecondCommon.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Students
+{
+    class SecondCommon<T> where T : IComparable<T>
+    {
+        public static void for_each_listbox(List<T> list, ListBox listBox, ref int oldSelect, ref int selected)
+        {
+            if (oldSelect == -1)
+            {
+                if ((oldSelect = selected >= (oldSelect = listBox.Items.Count - 1) >> 1 ? oldSelect : 0) == 0)
+                {
+                    list.setStart();
+                }
+                else
+                {
+                    list.setEnd();
+                }
+            }
+            for (; selected < oldSelect; --oldSelect, --list) ;
+            for (; selected > oldSelect; ++oldSelect, ++list) ;
+        }
+    }
+}
