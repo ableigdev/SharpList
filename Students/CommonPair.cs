f524f2e99aff87fd188a392f1a1470edec181630
ableigdev <igor.yarovoy.98@ukr.net>
2019-03-22 22:55:53 +0200

    Add ctors

--- Students/CommonPair.cs
+++ Students/CommonPair.cs
@@ -11,6 +11,17 @@ namespace Students
         public string m_Subject;
         public float m_Mark;
 
+        public CommonPair()
+        {
+
+        }
+
+        public CommonPair(string subject, float mark)
+        {
+            m_Subject = subject;
+            m_Mark = mark;
+        }
+
         int IComparable<CommonPair>.CompareTo(CommonPair pair)
         {
             return !Object.ReferenceEquals(pair, null) ? pair.m_Subject.CompareTo(m_Subject) : 1;
