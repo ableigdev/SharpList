e2f4fd7320208444ab2347821fe00d9d339a9c02
ableigdev <igor.yarovoy.98@ukr.net>
2019-03-22 12:44:39 +0200

    Add a common pair for the record book

---
+++ Students/CommonPair.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Students
+{
+    public class CommonPair : IComparable<CommonPair>
+    {
+        public string m_Subject;
+        public float m_Mark;
+
+        int IComparable<CommonPair>.CompareTo(CommonPair pair)
+        {
+            return !Object.ReferenceEquals(pair, null) ? pair.m_Subject.CompareTo(m_Subject) : 1;
+        }
+    }
+}
