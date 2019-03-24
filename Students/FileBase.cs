fdb7b5b09ff56109f355157b589920a4109a65a5
ableigdev <igor.yarovoy.98@ukr.net>
2019-03-24 22:32:18 +0200

    Add the first version the code for the working with files

---
+++ Students/FileBase.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Security.AccessControl;
+using System.Security;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace Students
+{
+    class FileBase
+    {
+        public const int DefBufSize = 4096;
+
+        /*
+        Описание внешнего метода на основе использования WinAPI функции 
+        для определения размера кластера на указанном разделе.
+        */
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true, EntryPoint = "GetDiskFreeSpaceW")]
+        static extern bool GetDiskFreeSpace(string lpRootPathName, out int lpSectorsPerCluster, out int lpBytesPerSector,
+            out int lpNumberOfFreeClusters, out int lpTotalNumberOfClusters);
+
+        // метод, вычисляющий размер кластера
+
+        public static bool GetClusterSize(string path, ref int BufSize)
+        {
+            int sectorsPerCluster;
+            int bytesPerSector;
+            int freeClusters;
+            int totalClusters;
+            BufSize = FileBase.DefBufSize;
+
+            if (GetDiskFreeSpace(Path.GetPathRoot(path), out sectorsPerCluster, out bytesPerSector, out freeClusters, out totalClusters))
+            {
+                BufSize = bytesPerSector * sectorsPerCluster;
+                return true;
+            }
+
+            return false;
+        }
+
+        // Статический метод, который открывает файл в синхронном или асинхронном режимах
+
+        public static bool OpenFile(ref FileStream fs, string FN, int BufLen, bool IsRead = true, bool IsOverl = false)
+        {
+            FileMode Mode = IsRead ? FileMode.Open : FileMode.Create;
+            try
+            {
+                /*
+                Это один из конструкторов с максимальным количеством аргументов. 
+                Его 1-ый аргумент  имя файла, 2-ой  режим работы с файлом, 
+                3-ий  режим совместного доступа; 4-ый длина буфера чтения; 
+                5-ый дополнительные опции, управляющие работой с файлом 
+                (указано FileOptions.SequentialScan, что задает оптимизацию 
+                при выполнении последовательных операций, 
+                а FileOptions.Asynchronous  асинхронный режим работы с файлом). 
+                При попытке открытия файлов возможно возникновения исключение, 
+                поэтому конструктор располагается в защищенной секции
+                */
+                fs = new FileStream(FN, Mode, IsRead ? FileSystemRights.ReadData : FileSystemRights.WriteData,
+                                    FileShare.None, BufLen,
+                                    FileOptions.SequentialScan | (IsOverl ? FileOptions.Asynchronous : FileOptions.None));
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
+        }
+
+        // Метод закрытия файла
+        public static void CloseFile(ref FileStream fs)
+        {
+            if (fs != null)
+            {
+                fs.Close();
+                fs = null;
+            }
+        }
+    }
+}
---
+++ Students/FileProcess.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Students
+{
+    class FileProcess<T> : FileSimple
+    {
+        // Описание делегатов, используемых для выполнения обработки прочитанных данных
+        public delegate void ProcessBuf<Type>(byte[] Buf, int BufLen, ref Type CurVal);
+        public delegate void ProcessVal<Type>(Type CurVal, ref Type Val);
+
+        private bool IsOverl;
+
+        bool IsOverlapped
+        {
+            get
+            {
+                return IsOverl;
+            }
+        }
+
+        public enum Mode { first };
+        public FileProcess(string fileName, StructureInterface<T> interf, Mode mod)
+        {
+
+        }
+
+        /*
+        Класс, массив экземпляров которых используется при реализации асинхронного и синхронного режимов работы
+        */
+        public class StateObject
+        {
+            // буфер для чтения данных
+            public byte[] Buf;
+            // экземпляр класса FileStream, используется только для асинхронного режима
+            public FileStream fs;
+            /*
+            Логическое значение, показывающее завершение отложенной операции ввода-вывода. Используется только для асинхронного режима. Ключевое слово volatile используется для отключения попыток компилятора при оптимизации сохраниться значение поля в регистре. Это необходимо для корректной работы release версии проекта 
+            */
+            public volatile bool EndInOut;
+            // Количество прочитанных из потока байт
+            public int ReadBytes;
+
+            // Конструктор с параметрами
+            public StateObject(int BufSz, FileStream fsVal)
+            {
+                Buf = new byte[BufSz];
+                fs = fsVal;
+            }
+
+        }
+
+        // Массив объектов состояния
+        StateObject[] States = null;
+
+        // Метод инициализации массива объектов состояния
+
+        private void InitBufs(bool IsOverl = false)
+        {
+            // Количество объектов состояния массива зависит от режима работы.
+            int NStates = IsOverl ? 2 : 1;
+            States = new StateObject[NStates];
+            /*
+            // Для повышения скорости работы в асинхронном режиме можно использовать 
+            // закомментированные строки кода
+                        if (IsOverl)
+                            BufLen <<= 4;
+             */
+            // Инициализация элементов массива
+            for (; NStates > 0; NStates--)
+                States[NStates - 1] = new StateObject(BufLen, fIn);
+        }
+
+        /*
+        Конструктор с параметрами. 1-ый аргумент  имя файла, 2-ой режим работы: false  синхронный, а true  асинхронный
+        */
+        public FileProcess(string FNIn, bool IsOverl = false)
+        {
+            this.IsOverl = IsOverl;
+            if (Init(FNIn, IsOverl))
+                InitBufs(IsOverl);
+        }
+    }
+}
